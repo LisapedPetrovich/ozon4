d83415cac31ce09bfd2285d436b6cd28a1230a70
LisapedPetrovich <xadelex@yandex.ru>
2020-01-08 22:30:44 +0300

    Fixing some inaccuracies

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.util.List;
 import java.util.Random;
 
@@ -17,15 +16,17 @@ public class NewTest {
     String cost4;
     String name4;
 
-    @Test(priority=1)
+    @Test(priority = 1)
+    //1. Открываем в браузере сайт https://www.ozon.ru/.
     public void test1() {
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\1\\Downloads\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\1\\IdeaProjects\\ozon4\\src\\main\\resources\\geckodriver.exe");
         driver = new FirefoxDriver();
         String url1 = "https://www.ozon.ru";
         driver.get(url1);
     }
 
-    @Test(priority=2)
+    @Test(priority = 2)
+    //2. В каталоге товаров выбираем категорию "Музыка".
     public void test2() throws InterruptedException {
         WebElement btn0 = driver.findElement(By.cssSelector("button.b0q6"));
         btn0.click();
@@ -34,40 +35,46 @@ public class NewTest {
         Thread.sleep(3000);
     }
 
-    @Test(priority=3)
+    @Test(priority = 3)
+    //3. Выбираем подкатегорию "Виниловые пластинки".
     public void test3() throws InterruptedException {
-        WebElement btn2 = driver.findElement(By.cssSelector("div.a2v3:nth-child(1) > a:nth-child(1)"));
+        WebElement btn2 = driver.findElement(By.cssSelector("div.a2v3:nth-child(1) > .a2v4"));
         btn2.click();
-        Thread.sleep(3000);
+        Thread.sleep(5000);
     }
 
-    @Test(priority=4)
+    @Test(priority = 4)
+    //4. Проверяем открывшийся список товаров.
     public void test4() {
         WebElement chck1 = driver.findElement(By.cssSelector("div.a3d0:first-child"));
         Assert.assertTrue(chck1.isDisplayed());
     }
 
-    @Test(priority=5)
+    @Test(priority = 5)
+    //5. Определяем количество товаров на странице.
     public void test5() {
         WebElement btn3 = driver.findElement(By.cssSelector("div.a3d0:last-child"));
         goodsSum = driver.findElements(By.cssSelector("div.a3d0")).size();
         System.out.println(goodsSum);
     }
 
-    @Test(priority=6)
+    @Test(priority = 6)
+    //6. Генерируем случайное число в диапазоне от 1 до значения количества товаров на странице.
     public void test6() {
-        rnd = 1 + (int) (Math.random()*goodsSum);
+        rnd = 0 + (int) (Math.random()*goodsSum);
         System.out.println(rnd);
     }
 
-    @Test(priority=7)
+    @Test(priority = 7)
+    //7. Выбираем товар со случайным порядковым номером, полученным в test6.
     public void test7() throws InterruptedException {
         List<WebElement> goodsList = driver.findElements(By.cssSelector("div.a3d0"));
         goodsList.get(rnd).click();
         Thread.sleep(5000);
     }
 
-    @Test(priority=8)
+    @Test(priority = 8)
+    //8. Запоминаем стоимость и название выбранного товара.
     public void test8() {
         WebElement cost1 = driver.findElement(By.cssSelector("span.b6w7"));
         String cost2 = cost1.getText();
@@ -75,7 +82,8 @@ public class NewTest {
         String name2 = name1.getText();
     }
 
-    @Test(priority=9)
+    @Test(priority = 9)
+    //9. Добавляем выбранный товар в корзину.
     public void test9() throws InterruptedException {
         WebElement btn4 = driver.findElement(By.cssSelector("button.close"));
         btn4.click();
@@ -84,7 +92,8 @@ public class NewTest {
         Thread.sleep(5000);
     }
 
-    @Test(priority=10)
+    @Test(priority = 10)
+    //10. Переходим в корзину и проверяем наличие товара в корзине.
     public void test10() throws InterruptedException {
         WebElement btn6 = driver.findElement(By.cssSelector("svg.bu9"));
         btn6.click();
@@ -94,6 +103,7 @@ public class NewTest {
     }
 
     @Test(priority = 11)
+    //11. Возвращаемся на страницу "Виниловые пластинки".
     public void test11() throws InterruptedException {
         driver.navigate().back();
         Thread.sleep(5000);
@@ -102,12 +112,14 @@ public class NewTest {
     }
 
     @Test(priority = 12)
+    //12. Генерируем случаное число в диапазоне от 1 до згачения, полученного в test5.
     public void test12() {
-        rnd2 = 1 + (int) (Math.random()*goodsSum);
+        rnd2 = 0 + (int) (Math.random()*goodsSum);
         System.out.println(rnd2);
     }
 
     @Test(priority = 13)
+    //13. Выбираем товар со случайным порядковым номером, полученным в test12.
     public void test13() throws InterruptedException {
         List<WebElement> goodsList = driver.findElements(By.cssSelector("div.a3d0"));
         goodsList.get(rnd2).click();
@@ -115,6 +127,7 @@ public class NewTest {
     }
 
     @Test(priority = 14)
+    //14. Запоминаем стоимость и название выбранного товара.
     public void test14() {
         WebElement cost3 = driver.findElement(By.cssSelector("span.b6w7"));
         String cost4 = cost3.getText();
@@ -123,6 +136,7 @@ public class NewTest {
     }
 
     @Test(priority = 15)
+    //15. Добавляем выбранный в test13 товар в корзину.
     public void test15() throws InterruptedException {
         WebElement btn7 = driver.findElement(By.cssSelector(".b6d5 > button:nth-child(1) > div:nth-child(1)"));
         btn7.click();
@@ -130,18 +144,18 @@ public class NewTest {
     }
 
     @Test(priority = 16)
+    //16. Проверяем наличие товаров в корзине без перехода в корзину.
     public void test16() {
         WebElement chck3 = driver.findElement(By.cssSelector("span.f-caption--bold"));
-        String chck4 = chck3.getText();
-        if (chck4 == "\n" +
-                "      2\n" +
-                "    ")
+        String chck4 = chck3.getAttribute("innerText");
+        if (chck4.equals("2"))
             System.out.println("Test passed.");
         else
             System.out.println("Test failed");
     }
 
     @Test(priority = 17)
+    //17. Открываем корзину.
     public void test17() throws InterruptedException {
         WebElement btn6 = driver.findElement(By.cssSelector("svg.bu9"));
         btn6.click();
@@ -149,18 +163,20 @@ public class NewTest {
     }
 
     @Test(priority = 18)
+    //18. Проверяем наличие добавленных ранее товаров в корзине и их общую стоимость.
     public void test18() {
         WebElement chck5 = driver.findElement(By.cssSelector("div.a8o1:nth-child(4) > div:nth-child(3) > a:nth-child(1) > span:nth-child(1)"));
         String chck6 = chck5.getText();
         WebElement chck7 = driver.findElement(By.cssSelector("div.a8o1:nth-child(3) > div:nth-child(3) > a:nth-child(1) > span:nth-child(1)"));
         String chck8 = chck7.getText();
-        if (chck6 == name2 && chck8 == name4)
+        if (chck6.equals(name2) && chck8.equals(name4))
             System.out.println("Test passed");
         else
             System.out.println("Test failed");
     }
 
     @Test(priority = 19)
+    //19. Удаляем из корзины все товары.
     public void test19() throws InterruptedException {
         WebElement btn8 = driver.findElement(By.cssSelector("span.a6p4"));
         btn8.click();
@@ -171,12 +187,14 @@ public class NewTest {
     }
 
     @Test(priority = 20)
+    //20. Проверяем, что корзина пуста.
     public void test20() {
         WebElement chck9 = driver.findElement(By.cssSelector("div.ui-a3:nth-child(1)"));
         Assert.assertTrue(chck9.isDisplayed());
     }
 
     @Test(priority = 21)
+    //21. Закрываем браузер.
     public void test21() {
         driver.quit();
     }
